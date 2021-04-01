03c419b15c6205d3b961070e141d02008f8a597c
DavidITSchool <david.dan@itschool.ro>
2021-04-01 19:46:49 +0300

    feat: add client model

---
+++ Model/client_model.py
@@ -0,0 +1,92 @@
+from os.path import exists
+
+from sqlalchemy import text
+
+from Domain.client import Client
+from utils import engine
+
+
+class ClientFileModel:
+    def __init__(self, filename):
+        self.__filename = filename
+        self.__all_clients = []
+        self.read_clients()
+
+    def get_all_clients(self):
+        return self.__all_clients
+
+    def create_client(self, cnp, last_name, first_name, email):
+        client = Client(
+            cnp=cnp,
+            last_name=last_name,
+            first_name=first_name,
+            email=email
+        )
+
+        self.__all_clients.append(client)
+        self.__save_to_file()
+
+    def read_clients(self):
+        """
+
+        :return: 
+        """
+        if not exists(self.__filename):
+            return
+
+        with open(self.__filename, 'r') as my_file:
+            for line in my_file:
+                line = line.strip().split('|')
+                client = Client(
+                    cnp=line[0],
+                    last_name=line[1],
+                    first_name=line[2],
+                    email=line[3]
+                )
+
+                self.__all_clients.append(client)
+
+    def __save_to_file(self):
+        """
+
+        """
+        with open(self.__filename, 'w') as my_file:
+            for client in self.__all_clients:
+                line = ''
+                line += str(client.get_cnp()) + '|' + client.last_name + '|' + client.first_name + '|' + \
+                        client.email + '\n'
+                my_file.write(line)
+
+    def update_client(self, cnp, last_name, first_name, email):
+        """
+
+        """
+        for index, client in enumerate(self.__all_clients):
+            if client.get_cnp() == cnp:
+                if last_name:
+                    self.__all_clients[index].last_name = last_name
+                if first_name:
+                    self.__all_clients[index].first_name = first_name
+                if email:
+                    self.__all_clients[index].email = email
+                break
+        self.__save_to_file()
+
+    def delete_client(self, cnp):
+        for index, client in enumerate(self.__all_clients):
+            if client.get_cnp() == cnp:
+                del self.__all_clients[index]
+
+        self.__save_to_file()
+
+    def find_by_id(self, cnp):
+        for client in self.__all_clients:
+            if client.get_cnp() == cnp:
+                return client
+        return None
+
+    def id_exists(self, cnp):
+        for client in self.__all_clients:
+            if client.get_cnp() == cnp:
+                return True
+        return False
